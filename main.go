04e496253830d8685c466031c4de04e74bd39e21
lvguangxing <lvguangxing@baidu.com>
2019-06-19 15:03:17 +0800

    feat(set rest api )

--- main.go
+++ main.go
@@ -31,8 +31,8 @@ func secp256k1Decode(privkey []byte) (pubkey []byte) {
 	return pubkey
 }
 
-//New Decode
-func DecodeKeyInput(w http.ResponseWriter, r *http.Request) {
+//secp256k1 handle function
+func DecodeSecp256k1Input(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var keyInput KeyInput
 	_ = json.NewDecoder(r.Body).Decode(&keyInput)
@@ -40,14 +40,26 @@ func DecodeKeyInput(w http.ResponseWriter, r *http.Request) {
 	KeyInputs = append(KeyInputs, keyInput)
 	seckey, _ := hex.DecodeString(keyInput.Key)
 	pubkey := secp256k1Decode(seckey)
-	hash := sha256.Sum256(pubkey)
-	json.NewEncoder(w).Encode(hash[:])
+	json.NewEncoder(w).Encode(pubkey)
+}
+
+//sha256 handle function
+func DecodeSha256Input(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var keyInput KeyInput
+	_ = json.NewDecoder(r.Body).Decode(&keyInput)
+	keyInput.ID = strconv.Itoa(R.Intn(100))
+	KeyInputs = append(KeyInputs, keyInput)
+	seckey, _ := hex.DecodeString(keyInput.Key)
+	res := sha256.Sum256(seckey)
+	json.NewEncoder(w).Encode(res)
 }
 
 // main the function where execution of the program begins
 func main() {
 	//Init router
 	r := mux.NewRouter()
-	r.HandleFunc("/api/KeyInputs", DecodeKeyInput).Methods("POST")
+	r.HandleFunc("/api/secp256k1", DecodeSecp256k1Input).Methods("POST")
+	r.HandleFunc("/api/sha256", DecodeSha256Input).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
